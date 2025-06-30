6805737dc342c89eaa59d98265d8633fb5fb84bb
Jit <jitrin.u@gmail.com>
2025-06-30 15:10:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const webYT = document.getElementById("ytID");
+webYT.addEventListener("click", function() {window.open("https://www.youtube.com", "_blank")});
+
+const webFB = document.getElementById("fbID");
+webFB.addEventListener("click", function() {window.open("https://www.facebook.com", "_blank")})
+
+const webTT = document.getElementById("ttID");
+webTT.addEventListener("click", function() {window.open("https://www.tiktok.com", "_blank")})
+
+const webIG = document.getElementById("igID");
+webIG.addEventListener("click", function() {window.open("https://www.instagram.com", "_blank")})
+
+const webX = document.getElementById("xID");
+webX.addEventListener("click", function() {window.open("https://www.x.com", "_blank")})
+
+// function clickYT() {
+//     window.open("https://www.youtube.com", "_blank");
+// }
+
+// document.getElementById("ytID").addEventListener("click", clickYT);
+
+// function clickFB() {
+//     window.open("https://www.facebook.com", "_blank");
+// }
+
+// document.getElementById("fbID").addEventListener("click", clickFB);
+
+// function clickTT() {
+//     window.open("https://www.tiktok.com", "_blank");
+// }
+
+// document.getElementById("ttID").addEventListener("click", clickTT);
+
+// function clickIG() {
+//     window.open("https://www.instagram.com", "_blank");
+// }
+
+// document.getElementById("igID").addEventListener("click", clickIG);
+
+// function clickX() {
+//     window.open("https://www.x.com", "_blank");
+// }
+
+// document.getElementById("xID").addEventListener("click", clickX);
